8217a43accc45e4ca0e3cd4c93a004d5cdf8c22c
hachispin <ganalonrainier255@gmail.com>
2026-05-25 14:18:35 +0100

    Tidy up this script a little
    - Remove unnecessary quoting in `[[  ]]` blocks
    - Add some more comments
    - Check for all programs used (instead of just `asusctl` and `ratbagctl`)
    - Check that the desktop environment is KDE

--- sync_leds_with_accent.sh
+++ sync_leds_with_accent.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
-# NOTE: You must set "Colours" to "Accent colour from wallpaper" in System Settings.
+# This script is meant to be used with the "Accent colour from wallpaper"
+# option toggled (in the Colours section), but still works regardless.
+#
+# NOTE: Your Logitech keyboard profiles (profile 0) will be changed!
 
-logitech_keyboard='Logitech G915 WIRELESS RGB MECHANICAL GAMING KEYBOARD'
+keyboard='Logitech G915 WIRELESS RGB MECHANICAL GAMING KEYBOARD'
 
-# $1: program name
-program_exists() {
-	command -v "$1" &>/dev/null
-}
-
-if ! program_exists 'asusctl' || ! program_exists 'ratbagctl'; then
-	echo 'ERROR: must have both asusctl and ratbagctl in PATH' >&2
-	exit 127
+if [[ $XDG_CURRENT_DESKTOP != 'KDE' ]]; then
+	echo 'ERROR: only KDE is supported' >&2
+	exit 1
 fi
+
+for program in 'kreadconfig6' 'inotifywait' 'asusctl' 'ratbagctl'; do
+	if ! command -v "$program" &>/dev/null; then
+		echo "ERROR: $program is not installed and in PATH" >&2
+		exit 1
+	fi
+done
 
 # Returns the accent color in hexadecimal.
 #
@@ -27,11 +32,11 @@ get_accent_hex() {
 #
 # $1: color in hexadecimal (e.g., ffffff)
 set_colors() {
-	if ratbagctl "$logitech_keyboard" profile 0 get &>/dev/null; then
-		ratbagctl "$logitech_keyboard" profile 0 led 0 set color "$1" # Logo
-		ratbagctl "$logitech_keyboard" profile 0 led 1 set color "$1" # Keys
+	if ratbagctl "$keyboard" profile 0 get &>/dev/null; then
+		ratbagctl "$keyboard" profile 0 led 0 set color "$1" # Logo
+		ratbagctl "$keyboard" profile 0 led 1 set color "$1" # Keys
 	else
-		echo "Failed to get profile; assuming keyboard is disconnected" >&2
+		echo "Failed to get profile 0; assuming keyboard is disconnected" >&2
 	fi
 
 	# Setting color seems to reset brightness to "high"
@@ -40,7 +45,7 @@ set_colors() {
 	level=${level,,}
 
 	# To prevent annoying flicker if the keyboard is off
-	if [[ $level == "off" ]]; then
+	if [[ $level == 'off' ]]; then
 		asusctl aura power-tuf --awake false --keyboard
 		asusctl aura effect static -c "$1"
 		asusctl leds set "$level"
@@ -59,11 +64,11 @@ set_colors "$last_accent"
 
 # Don't watch the file (kdeglobals) itself because of renames.
 while read -r _dir _event file; do
-	if [[ "$file" == "kdeglobals" ]]; then
+	if [[ $file == 'kdeglobals' ]]; then
 		accent="$(get_accent_hex)"
 
 		# Only call set_colors() when the color changes.
-		if [[ "$accent" != "$last_accent" ]]; then
+		if [[ $accent != "$last_accent" ]]; then
 			set_colors "$accent"
 			last_accent="$accent"
 		fi
