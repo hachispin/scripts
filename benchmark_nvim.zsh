351d2e307bf30e6449dc4ee3962bdf3044868131
hachispin <ganalonrainier255@gmail.com>
2026-06-15 19:00:28 +0100

    use `repeat` syntax

--- benchmark_nvim.zsh
+++ benchmark_nvim.zsh
@@ -5,7 +5,7 @@ set -euo pipefail
 sum_times=0.0
 num_iterations=25
 
-for ((i = 0; i < num_iterations; i++)); do
+repeat $num_iterations; do
 	output=$(nvim --headless --startuptime /dev/stdout +q!)
 	final_time=$(tail -n2 <<<"$output" | tr -d '\n' | cut -d ' ' -f1 | sed 's/^0*//')
 	((sum_times += final_time))
